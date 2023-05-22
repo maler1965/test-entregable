427fb6e62237a0fe364906c027a7fa41bab1dada
PCpedroCevallos <pevallos1965@gmail.com>
2023-05-22 13:50:22 -0500

    Se ajusto el estilo del boton, se añadio sombra y se le dio animacion a la sombra. El puntero cambia de forma al estar sobre el boton. Se añadio un nuevo componente, el Footer.jsx

---
+++ src/componets/Footer.jsx
@@ -0,0 +1,17 @@
+
+import React from 'react'
+
+const Footer = ({ author }) => {
+    return (
+        <div>
+            <footer className='app__footer'>
+                <h3> Author: {author} </h3>
+            </footer>
+
+        </div>
+    )
+}
+
+export default Footer
+
+
